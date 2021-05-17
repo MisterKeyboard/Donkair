f4b93bc87f304b8b6616a029571c71bfd313dcbf
lena <lenamartin@outlook.fr>
2021-05-17 10:50:41 +0200

    dispkayFullFlights

--- searchflight.php
+++ searchflight.php
@@ -5,6 +5,37 @@
 //require_once "espaceAdmin/db.php";
 // <!-- QUERY POUR RECHERER UN VOL  -->
 
+$allFlights = '
+SELECT 
+    flight.id id,
+    plane.capacity capacity,
+    COUNT(booking.name) nbrCustomer, 
+(plane.capacity - COUNT(booking.name)) placeDispo
+FROM flight
+INNER JOIN plane ON plane.id = flight.planeModel
+LEFT JOIN booking ON booking.flightId = flight.id
+GROUP BY flight.id;
+';
+$resultat = $objetPdo->query($allFlights);
+
+
+while($row = $resultat->fetch(PDO::FETCH_ASSOC)) {
+$idF= $row['id']; 
+$nbrCustomer = $row['capacity'];
+$capacity = $row['nbrCustomer']; 
+$placeDispo = $row['placeDispo'];
+//echo "<br>";
+
+
+if ($placeDispo <= 0) {
+ echo "le vol " . $idF . " est complet" . "<br>";
+} ;
+}
+
+//foreach()
+
+
+
 if (!empty($_POST)) {
 
     $request = '
@@ -69,42 +100,6 @@ if (!empty($_POST)) {
     $departureCity = $_POST['arrivalCity'];
     $arrivalCity = $_POST['departureCity'];
 
-
-    ?>
-
-    <!-- <div class="container pt-3 recherche:">
-        <h2 class="text-primary fw-bold">Votre recherche </h2>
-    </div>
-    <div class="container card my-3 pb-3">
-        <div class="row">
-            <div class="col-sm-12 col-md-6 col-lg-6 pt-3">
-                <img src="img/uploadtownsimages/<?php //echo $departureImage ?> " class="card-img-top" alt="photo ville">
-                <p><span class="text-primary">Ville de départ: </span><?php //echo $departureCity ?></p>
-            </div>
-            <div class="col-sm-12 col-md-6 col-lg-6 pt-3">
-                <img src="img/uploadtownsimages/<?php //echo $arrivalImage ?> " class="card-img-top" alt="photo ville">
-                <p><span class="text-primary">Ville d'arrivée: </span><?php //echo $arrivalCity ?></p>
-            </div>
-        </div>
-        <h3 class="text-primary">Validez votre vol</h3>
-  <?php  
-    
-?>
-        <!-flip card
-        <div class="flip-card">
-  <div class="flip-card-inner">
-    <div class="flip-card-front">
-    <img src="">
-    </div>
-    <div class="flip-card-back">
-      <h1></h1>
-      <p></p>
-      <p></p>
-    </div>
-  </div>
-</div> -->
-
-<?php
     
 
     foreach ($flights as $flight)
@@ -139,109 +134,3 @@ if (!empty($_POST)) {
     </div>
 <?php 
     }
-
-    $count = '
-    SELECT 
-        flight.id id,
-        plane.capacity capacity,
-        COUNT(booking.name), 
-       (plane.capacity - COUNT(booking.name)) placeDispo
-    FROM flight
-    INNER JOIN plane ON plane.id = flight.planeModel
-    LEFT JOIN booking ON booking.flightId = flight.id
-    GROUP BY flight.id;
-    ';
-
-$flight = [
-    [
-    'id' => 1;
-    'start' => ''
-    'arrivée' =>']
-    ]
-
-$booking = [
-    [
-        flightID
-    ]
-    ]
-
-//isFree = $resa[$key]['freesi] > 0 ? 'oui' : 'non'; 
-     //indexer tab sur vol ID 
-
-     foreach($resas as $resa)
-$trueR['$re5' [$vols]] = $true []
-
-//1 => vide
-//2=> plein
-
-    $countFetch = $objetPdo->query($count);
-    //$items as $key => value
-    //Recupe $resa [$vol][id] = $resa['capacity']
-    foreach ($countFetch as $row) {
-        $idF= $row['id']; 
-        $capacity = $row['capacity']; 
-        $placeDispo = $row['placeDispo'];
-    };
-
-
- //indexer tab sur vol ID 
-//for each($resas as $resa)
-$trueR['$re5' [$vols]] = $true []
-
-//1 => vide
-//2=> plein
-
-    $countFetch = $objetPdo->query($count);
-    //$items as $key => value
-    //Recupe $resa [$vol][id] = $resa['capacity']
-    foreach ($countFetch as $row) {
-        $idF= $row['id']; 
-        $capacity = $row['capacity']; 
-        $placeDispo = $row['placeDispo'];
-//REVOIR TABLEAU
-
-
-
-//indexer
-$trueREsa = [];
-foreach($resas as $resa) {
-    $trueResa[$resa['flightId']] = $resa
-}
-
-
-foreach ($flight as $key=> $flight) {
-    $flight['id']
-    $flight['statrt']
-    $trtrueResa[$flight[id]]['freesits']
-}
-
-
-
-
-
-// $id = $_GET['idd']; 
-
-// $sql = "SELECT flightNbr FROM flight WHERE id=?";
-
-// $stmt = $objetPdo->prepare($sql);
-
-// $stmt->bindValue(1, $id, PDO::PARAM_INT);
-
-// $stmt->execute();
-
-// $row = $stmt->fetch(PDO::FETCH_OBJ);
-    
-// $i = $_GET['idd'];
-
-// $i = $_GET['idd'];
-// $title = $_GET['title'];
-
-// $sql = "UPDATE flight SET flightNbr=:flightNbr, WHERE id=:i";
-
-// $sql= $objetPdo->prepare($sql);
-
-
-// $sql->bindValue('i', $i, PDO::PARAM_INT);
-// $sql->bindValue('flightNbr', $flightNbr, PDO::PARAM_STR);
-
-// $sql= $sql->execute();
